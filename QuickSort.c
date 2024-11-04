e30a719801492003934f6b8ac0669a77953b2a23
Mathhuang01 <matheushuang2006@gmail.com>
2024-11-04 19:28:25 -0300

    Create QuickSort.c

---
+++ QuickSort.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void swap(struct Node* a, struct Node* b) {
+    int temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+
+struct Node* partition(struct Node* low, struct Node* high) {
+    int pivot = high->data; // Pivô
+    struct Node* i = low->next;  // Ponteiro para o menor elemento
+
+    struct Node* j = low;
+    while (j != high) {
+        if (j->data < pivot) {
+            swap(j, i);
+            i = i->next;
+        }
+        j = j->next;
+    }
+    swap(i, high);
+    return i;
+}
+
+void quickSort(struct Node* low, struct Node* high) {
+    if (high != NULL && low != high && low != high->next) {
+        struct Node* pi = partition(low, high);
+        quickSort(low, pi->next);
+        quickSort(pi->next, high);
+    }
+}
+
+struct Node* getLastNode(struct Node* head) {
+    while (head != NULL && head->next != NULL) {
+        head = head->next;
+    }
+    return head;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    push(&head, 10);
+    push(&head, 80);
+    push(&head, 30);
+    push(&head, 90);
+    push(&head, 40);
+    push(&head, 50);
+    push(&head, 70);
+
+    printf("Lista antes da ordenação:\n");
+    printList(head);
+
+    struct Node* last = getLastNode(head);
+    quickSort(head, last);
+
+    printf("Lista após a ordenação:\n");
+    printList(head);
+
+    return 0;
+}
